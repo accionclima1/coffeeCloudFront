d7aea27bdd38b32c4529ca144f3ac60d9ba3e8ea
Augusto Valerio <auguvalerio@gmail.com>
2017-05-17 12:28:17 -0600

    url update

--- www/javascripts/global.js
+++ www/javascripts/global.js
@@ -1,7 +1,7 @@
 ﻿var global = {};
 global.setting = {
-    //service: "http://coffeecloud.centroclima.org",
-    service: "http://coffeecloud.centroclima.org",
+    //service: "http://coffeecloud.centroclima.org/",
+    service: "http://coffeecloud.centroclima.org/",
     getServiceUrl: function () {
         return this.service;
     }
